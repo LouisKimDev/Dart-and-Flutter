270514b3da3c6a8366141d555eec4446d98b6b60
DoyunKim <louiskim.dev@gmail.com>
2023-03-10 18:19:26 +0900

    Assignment1_personal_info

---
+++ lib/Assignment/ch01_labs_5428362/personal_info.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  print("*************************************\n");
+  print("Name: Doyun Kim\n");
+  print("ID: 5428362\n");
+  print("Phone: 010 3561 4217\n");
+  print("Department: College of Engineering\n");
+  print("Grade: 4\n");
+  print("*************************************\n");
+}
